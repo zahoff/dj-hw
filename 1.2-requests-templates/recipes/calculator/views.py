d2e34ef4cef14d7c106563d84285870e981bb3ac
zahoff <114179073+zahoff@users.noreply.github.com>
2023-02-20 18:33:39 +0500

    Add files via upload

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -17,14 +17,30 @@ DATA = {
         'помидор, ломтик': 1,
     },
     # можете добавить свои рецепты ;)
+    'soup puree': {
+        'картофель, шт': 3,
+        'морковь, шт': 3,
+        'лук, шт': 3,
+        'тыква, г': 300,
+        'сливки, г': 200,
+        'паприка, ч.л.': 0.5,
+        'соль, ч.л.': 0.5,
+        'масло подсолнечное, ст.л.': 2,
+        'вода, л': 1,
+    },
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def calculate_view(requests, dish_get):
+    servings = int(requests.GET.get('servings', 1))
+    send_dish = {}
+    for dish, ingredients in DATA.items():
+        if dish_get == dish:
+            for ing, count in ingredients.items():
+                send_dish[ing] = count * servings
+
+    context = {
+        'recipe': send_dish
+    }
+
+    return render(requests, 'calculator/index.html', context)

61d57376873b103070d4e60fabf0e737db4cdd2a
zahoff <114179073+zahoff@users.noreply.github.com>
2023-02-25 11:51:20 +0500

    Add files via upload

---
+++ 2.1-databases/work_with_database/phones/migrations/0002_alter_phone_slug.py
@@ -0,0 +1,15 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('phones', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='phone',
+            name='slug',
+            field=models.SlugField(),
+        ),
+    ]

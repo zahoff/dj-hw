ba45765c53ed5bcb65a5ce8fc6338e831762f3b3
zahoff <114179073+zahoff@users.noreply.github.com>
2023-03-06 02:49:10 +0500

    Add files via upload

--- 3.3-permissions/api_with_restrictions/api_with_restrictions/urls.py
+++ 3.3-permissions/api_with_restrictions/api_with_restrictions/urls.py
@@ -1,5 +1,4 @@
 """api_with_restrictions URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
@@ -18,8 +17,11 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
+from advertisements.views import AdvertisementViewSet
+
 router = DefaultRouter()
 # TODO: подключите `AdvertisementViewSet`
+router.register('advertisements', AdvertisementViewSet)
 
 
 urlpatterns = [

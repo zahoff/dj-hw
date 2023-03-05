9ee322f8e696865d122d85c0b4be32259362e4ff
zahoff <114179073+zahoff@users.noreply.github.com>
2023-03-06 01:54:37 +0500

    Add files via upload

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -5,12 +5,14 @@ from logistic.serializers import ProductSerializer, StockSerializer
 
 from rest_framework.filters import SearchFilter
 
+class CustomSearchFilter(SearchFilter):
+    search_param = 'products'
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # при необходимости добавьте параметры фильтрации
-    filter_backends = [SearchFilter]
+    filter_backends = [CustomSearchFilter]
     search_fields = ['title', 'description']
 
 
@@ -18,6 +20,5 @@ class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     # при необходимости добавьте параметры фильтрации
-    filter_backends = [SearchFilter]
-    SearchFilter.search_param = 'products'
+    filter_backends = [CustomSearchFilter]
     search_fields = ['products__title', 'products__description']
